c11637cdbcfaf8b2b37c75a43c5906674b19d465
Bjornskjald <github@bjorn.ml>
2018-10-07 12:55:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# make-mcd-mail
+
+Node.js app for replacing regular mails with free coupons from McDonalds for Poland
+
+Aplikacja Node.js, która zamienia zwykłe maile w darmowe kupony do McDonalda
+
+**Wymagane: własny serwer mailowy, Maildir**
+
+**Setup:**
+- Move/copy config.example.json to config.json
+- Fill it with needed info
+- Run `node index.js` with a mail file name as a first argument
+
+**Konfiguracja:**
+- Przenieś/skopiuj config.example.json do config.json
+- Wypełnij go informacjami
+- Uruchom `node index.js` z nazwą pliku maila jako pierwszy argument
---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const config = require('./config.json')
+
+const randomstring =
+  '06y7' +
+  '3' + Math.random().toString(36).substr(2, 3) +
+  Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4)
+
+const path = config + '/cur/' + process.argv[2]
+
+let mail = fs.readFileSync(path, 'utf-8')
+  .replace('From: ' + config.from, 'From: "Badanie.Satysfakcji@ankieta.mcdonalds.pl" <Badanie.Satysfakcji@ankieta.mcdonalds.pl>')
+  .replace(config.body, `<html><table style="width:580px;"><tbody><tr><td colspan="2">Dziękujemy za
+udział w badaniu. Przesyłamy wybrany przez Pana/ią
+kupon.</td></tr><tr></tr><tr><td>DATA WYDANIA: ${new Date().toJSON().split('T')[0].split('-').reverse().join('-')}</td><td>UNIKALNY
+KOD: ${randomstring}</td></tr><tr><td colspan="2"><img
+src="https://echo.gfk.com/static/shared/gsc/echo/McDonalds/Poland/coupons/21_08_17_big_ice_cream.png"></td></tr></tbody></table></html>`)
+  .replace(`Content-Type: text/plain; charset=US-ASCII;
+ format=flowed`, 'Content-Type: text/html; charset=utf-8')
+  .replace('Content-Transfer-Encoding: 7bit', 'Content-Transfer-Encoding: 8bit')
+  .replace('Subject: ' + config.subject, 'Subject: =?utf-8?Q?McDonald=E2=80=99s?=')
+
+fs.writeFileSync(path, mail, 'utf-8')
