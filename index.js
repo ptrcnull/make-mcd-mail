b2abee6219ea85760376bc861773a8b6a3ef8e6d
Bjornskjald <github@bjorn.ml>
2018-11-19 14:24:18 +0100

    added checking if the mail has arrived

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const fs = require('fs')
 const config = require('./config.json')
 const sendMail = require('./sendMail')
 const getMailContent = require('./getMailContent')
+const getFiles = path => fs.readdirSync(path).slice(-10).filter(file => fs.readFileSync(path + file, 'utf8').includes('Subject: mcd_mailer'))
 
-sendMail().then(() => {
+sendMail().then(async () => {
   const randomstring =
     '06y7' +
     '3' + Math.random().toString(36).substr(2, 3) +
     Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4)
 
   const path = config.maildir + '/cur/'
-  const files = fs.readdirSync(path).slice(-10)
-  const file = files.filter(file => fs.readFileSync(path + file, 'utf8').includes('Subject: mcd_mailer'))[0]
+  let files = getFiles(path)
+
+  while (files.length === 0) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 100))
+    files = getFiles(path)
+  }
+
+  const file = files[0]
 
   let mail = fs.readFileSync(file, 'utf8')
     .replace('From: ' + config.from, 'From: "Badanie.Satysfakcji@ankieta.mcdonalds.pl" <Badanie.Satysfakcji@ankieta.mcdonalds.pl>')
